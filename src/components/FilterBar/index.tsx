da382c6c8e8b5772d7f4c33ccefa1bab1770e75f
Kip Price <4605379+kipprice@users.noreply.github.com>
2020-07-27 13:17:22 -0400

    update to new styles

--- src/components/FilterBar/index.tsx
+++ src/components/FilterBar/index.tsx
@@ -1,15 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import "./styles.scss";
 import { DisplayModeFilter } from "./DisplayMode";
 import { LevelFilters } from "./LevelFilters";
+import { EXPAND_COLLAPSE_ICON } from "../../helpers/constants";
+import cx from "classnames";
+import { CategoryFilters } from "./CategoryFilters";
 
 export const FilterBar: React.FC = () => {
+  const [isCollapsed, setIsCollapsed] = useState(false);
   return (
-    <div className="filterBar">
-      <div className="subtitle">Markdown-To-Matrix</div>
-      <div className="title">Markdown Explorer</div>
+    <div className={cx("filterBar", isCollapsed && "collapsed")}>
+      <h2 className="subtitle">Markdown-To-Matrix</h2>
+      <h1 className="title">Markdown Explorer</h1>
       <DisplayModeFilter />
       <LevelFilters />
+      <CategoryFilters />
+      <button
+        title="collapse the sidebar"
+        className="collapse"
+        onClick={() => setIsCollapsed(!isCollapsed)}
+      >
+        <img aria-hidden="true" src={EXPAND_COLLAPSE_ICON}></img>
+      </button>
     </div>
   );
 };
