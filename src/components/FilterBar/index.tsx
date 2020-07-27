57d40f8fdcc2deb77fca11a1f62ed0c3ea62ecdf
Kip Price <4605379+kipprice@users.noreply.github.com>
2020-07-27 12:59:13 -0400

    update fonts, sidebar, readme

--- src/components/FilterBar/index.tsx
+++ src/components/FilterBar/index.tsx
@@ -1,18 +1,15 @@
-import React from 'react';
-import './styles.scss';
-import { DisplayModeFilter } from './DisplayMode';
-import { LevelFilters } from './LevelFilters';
+import React from "react";
+import "./styles.scss";
+import { DisplayModeFilter } from "./DisplayMode";
+import { LevelFilters } from "./LevelFilters";
 
 export const FilterBar: React.FC = () => {
-
-    return (
-        <div className='filterBar'>
-            <div className='subtitle'>Competency Explorer</div>
-            <div className='title'>Codecademy Engineering</div>
-            <DisplayModeFilter />
-            <LevelFilters />
-            
-        </div>
-    );
-}
-
+  return (
+    <div className="filterBar">
+      <div className="subtitle">Markdown-To-Matrix</div>
+      <div className="title">Markdown Explorer</div>
+      <DisplayModeFilter />
+      <LevelFilters />
+    </div>
+  );
+};
